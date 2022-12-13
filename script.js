9a469958beaa9f2eb09379f3dc78b2be7ab8599f
timoxAxa <kushmamatovtimur@gmail.com>
2022-12-13 08:46:14 +0500

    first git work

---
+++ script.js
@@ -0,0 +1,20 @@
+// // 1
+// let list = 'alem napom d-low colaps'
+// let bbx = prompt('What is your name?')
+// if (bbx === 'alem' || bbx === 'napom' || bbx === 'd-low' || bbx === 'colaps') {
+//     console.log('welcome my brother. АССАЛЬЯМ АЛЯЙКУМ');
+// } else {
+//     console.log('go home fast. Быстро побежал БЕГОМММ');
+// }
+
+//2
+let int = prompt('Как вас зовут?')
+let years = confirm('Вам есть 18?')
+if (int.length % 2 === 0 && years === true) {
+    console.log('chetnoe');
+    console.log(years);
+} else {
+    console.log('nechetnoe');
+    console.log(years);
+}
+
